b670a64c548229774e5c9a8702f367f0c5fa022f
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-03-07 17:29:21 +0100

    Fer que el barco es pugi moure per tots els costats

--- VersioSocketGuillem/Html/Move.js
+++ VersioSocketGuillem/Html/Move.js
@@ -6,13 +6,21 @@ window.onload = function () {
   let positions = Array(imgs.length).fill(0);
   let moveLeft = true;
   let moveTop = false;
+  let moveRight = false;
+  let moveBottom = false;
 
   function animate() {
     for (let i = 0; i < imgs.length; i++) {
       if (moveLeft) {
         positions[i] += speed;
         imgs[i].style.left = positions[i] + "px";
-      } else {
+      } else if (moveBottom) {
+        positions[i] -= speed;
+        imgs[i].style.top = positions[i] + "px";
+      } else if (moveRight) {
+        positions[i] -= speed;
+        imgs[i].style.left = positions[i] + "px";
+      } else if (moveTop) {
         positions[i] += speed;
         imgs[i].style.top = positions[i] + "px";
       }
@@ -32,15 +40,38 @@ window.onload = function () {
     if (event.key === 's' || event.key === 'S') {
       moveLeft = false;
       moveTop = true;
+      moveRight = false;
+      moveBottom = false;
       for (let i = 0; i < imgs.length; i++) {
         imgs[i].style.transform = 'rotate(90deg)';
       }
-      
-
-    }
+    } else if (event.key === 'd' || event.key === 'D') {
+      moveLeft = true;
+      moveTop = false;
+      moveRight = false;
+      moveBottom = false;
+      for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.transform = 'rotate(0deg)';
+      }
+    }else if (event.key === 'a' || event.key === 'A') {
+      moveLeft = false;
+      moveTop = false;
+      moveRight = true;
+      moveBottom = false;
+      for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.transform = 'rotate(180deg)';
+      }
+    } else if(event.key === 'w' || event.key === 'W'){
+      moveLeft = false;
+      moveTop = false;
+      moveRight = false;
+      moveBottom = true;
+      for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.transform = 'rotate(270deg)';
+      }
     
    
-    
+    }
   });
  
 }
