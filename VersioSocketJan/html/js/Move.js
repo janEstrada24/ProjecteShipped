a0ea814190868d67cbe77fcaeff809162523520c
janEstrada24 <jestrada48@boscdelacoma.cat>
2024-03-07 17:40:39 +0100

    La imatge apareix pel costat contrari (dreta-esquerra o amunt-avall) quan sobrepassa la pantalla

--- VersioSocketJan/html/js/Move.js
+++ VersioSocketJan/html/js/Move.js
@@ -3,7 +3,6 @@ window.onload = function start() {
     var position = { left: 0, top: 0 };
     var speed = 1; 
 
-    // Objeto para rastrear qué teclas están presionadas
     var keysPressed = {
         "A": false,
         "D": false,
@@ -14,42 +13,40 @@ window.onload = function start() {
     function moveImage() {
         if (keysPressed["A"]) {
             position.left -= speed;
+            if (position.left < -image.offsetWidth) {
+                position.left = window.innerWidth;
+            }
         }
         if (keysPressed["D"]) {
             position.left += speed;
+            if (position.left > window.innerWidth) {
+                position.left = -image.offsetWidth;
+            }
         }
         if (keysPressed["W"]) {
             position.top -= speed;
+            if (position.top < -image.offsetHeight) {
+                position.top = window.innerHeight;
+            }
         }
         if (keysPressed["S"]) {
             position.top += speed;
+            if (position.top > window.innerHeight) {
+                position.top = -image.offsetHeight;
+            }
         }
         
         image.style.left = position.left + "px";
         image.style.top = position.top + "px";
+
         if (position >= window.innerWidth) {
             position = -image.width;
         }
         if (position >= window.innerHeight) {
             position = -image.height;
         }
+        
         requestAnimationFrame(moveImage);
-    }
-
-    function moveRight() {
-        position += speed;
-        image.style.left = position + "px";
-        if (position >= window.innerWidth) {
-            position = -image.width;
-        }
-    }
-
-    function moveLeft() {
-        position += speed;
-        image.style.right = position + "px";
-        if (position >= window.innerWidth) {
-            position = -image.width;
-        }
     }
 
     window.addEventListener("keydown", function(event) {
@@ -59,28 +56,6 @@ window.onload = function start() {
             }
             keysPressed[event.key.toUpperCase()] = true;
         }
-        /*if (event.key === "W" || event.key === "w") {
-            direction = "up";
-            moveImage();
-            console.log("Tecla 'W' o 'w' premuda");
-        }
-        else if (event.key === "A" || event.key === "a") {
-            direction = "left";
-            moveImage();
-            //setInterval(moveLeft, 10);
-            console.log("Tecla 'A' o 'a' premuda");
-        }
-        else if (event.key === "S" || event.key === "s") {
-            direction = "down";
-            moveImage();
-            console.log("Tecla 'S' o 's' premuda");
-        }
-        else if (event.key === "D" || event.key === "d") {
-            direction = "right";
-            moveImage();
-            //setInterval(moveRight, 10);
-            console.log("Tecla 'D' o 'd' premuda");
-        }*/
     });
 
     moveImage();
