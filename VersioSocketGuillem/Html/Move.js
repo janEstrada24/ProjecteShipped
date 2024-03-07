24188c421703ea631280a75016d7d65c8fc7142d
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-03-07 17:04:32 +0100

    Poder moure el vaixell cam a Vall amb la tecla s

--- VersioSocketGuillem/Html/Move.js
+++ VersioSocketGuillem/Html/Move.js
@@ -1,20 +1,46 @@
-window.onload = function() {
-  let imgs = document.querySelectorAll('img[src="/Images/VaixellVerdIBlau.jpg"]'); // select only the images with "VaixellVerdIBlau.jpg" in their src
-  let speed = 1; // speed of the movement
-  let positions = Array(imgs.length).fill(0); // initial positions for each image
+window.onload = function () {
+  let imgs = document.querySelectorAll(
+    'img[src="/Images/VaixellVerdIBlau.jpg"]'
+  );
+  let speed = 0.2;
+  let positions = Array(imgs.length).fill(0);
+  let moveLeft = true;
+  let moveTop = false;
 
   function animate() {
     for (let i = 0; i < imgs.length; i++) {
-      positions[i] += speed; // update position for each image
-      imgs[i].style.left = positions[i] + 'px'; // apply new position
+      if (moveLeft) {
+        positions[i] += speed;
+        imgs[i].style.left = positions[i] + "px";
+      } else {
+        positions[i] += speed;
+        imgs[i].style.top = positions[i] + "px";
+      }
 
       if (positions[i] > window.innerWidth) {
-        positions[i] = -imgs[i].offsetWidth; // reset position to start if image has moved off screen
+        positions[i] = -imgs[i].offsetWidth;
       }
     }
 
-    requestAnimationFrame(animate); // call animate again for the next frame
+    requestAnimationFrame(animate);
   }
 
   animate();
+
+
+  window.addEventListener('keydown', function (event) {
+    if (event.key === 's' || event.key === 'S') {
+      moveLeft = false;
+      moveTop = true;
+      for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.transform = 'rotate(90deg)';
+      }
+      
+
+    }
+    
+   
+    
+  });
+ 
 }
