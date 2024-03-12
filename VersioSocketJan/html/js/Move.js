7ce3f1f1e475595db9c3c67648d9a58a6bff637e
janEstrada24 <jestrada48@boscdelacoma.cat>
2024-03-12 19:00:11 +0100

    classe "imatge" canviada de nom a "vaixell" dins de "index.html". Variable "imgs" dins de "Move.js" canviada de nom a "vaixells". Per últim, l'alçada máxima on apareixeran barrils en una posició aleatòria modificada al 90% de l'alçada de la pantalla

--- VersioSocketJan/html/js/Move.js
+++ VersioSocketJan/html/js/Move.js
@@ -1,61 +1,73 @@
 window.onload = function () {
-    let imgs = Array.from(document.getElementsByClassName("imatge"));
+    let vaixells = Array.from(document.getElementsByClassName("vaixell"));
 
     let speed = 1;
     let activeImg = 0;
-    let degrees = new Array(imgs.length).fill(0);
-    let barrils = 10;
+    let degrees = new Array(vaixells.length).fill(0);
+    let barrils = Array.from(document.getElementsByClassName("barrils"));
+
+    let quantityBarrils = 10;
 
     const webSocket = new WebSocket("ws://172.23.1.129:3000");
     
+    /**
+     * Afegirem barrils en una posicio aleatoria de la pantalla
+     * tenint en compte l'alcada i la meitat de l'amplada. 
+     * Per altra part, es fara un radi sempre des del punt central
+     * de la pantalla.
+     * D'aquesta manera, els barrils mai apareixeran en una posicio
+     * on hi hagin vaixells.
+     */
     function addBarrils() {
         const windowWidth = window.innerWidth * 0.5;
-        const windowHeight = window.innerHeight;
+        const windowHeight = window.innerHeight * 0.9;
         const centerX = window.innerWidth / 2;
         const centerY = window.innerHeight / 2;
         var randomX;
         var randomY;
 
-        for (let i = 0; i < barrils; i++) {
-            let img = document.createElement("img");
-            img.src = "/Images/barril.png";
-            img.classList.add("barril");
-            img.style.position = "absolute";
+        for (let i = 0; i < quantityBarrils; i++) {
+            let barril = document.createElement("img");
+            barril.src = "/Images/barril.png";
+            barril.classList.add("barril");
+            barril.style.position = "absolute";
 
             randomX = centerX - (windowWidth / 2) + (Math.random() * windowWidth);
             randomY = centerY - (windowHeight / 2) +  (Math.random() * windowHeight);
-            img.style.left = randomX + "px";
-            img.style.top = randomY + "px";
-            document.body.appendChild(img);
+            barril.style.left = randomX + "px";
+            barril.style.top = randomY + "px";
+            document.body.appendChild(barril);
         }
     }
 
     function animate() {
-      imgs.forEach((img, index) => {
-        let imgWidth = img.offsetWidth;
-        let imgHeight = img.offsetHeight;
-        let currentLeft = img.offsetLeft;
-        let currentTop = img.offsetTop;
+      vaixells.forEach((vaixell, index) => {
+        let vaixellWidth = vaixell.offsetWidth;
+        let vaixellHeight = vaixell.offsetHeight;
+        let currentLeft = vaixell.offsetLeft;
+        let currentTop = vaixell.offsetTop;
 
         let radian = degrees[index] * (Math.PI / 180);
         currentLeft += speed * Math.cos(radian);
         currentTop += speed * Math.sin(radian);
   
         if (currentLeft > window.innerWidth) {
-          currentLeft = -imgWidth;
-        } else if (currentLeft < -imgWidth) {
+          currentLeft = -vaixellWidth;
+        } else if (currentLeft < -vaixellWidth) {
           currentLeft = window.innerWidth;
         }
   
         if (currentTop > window.innerHeight) {
-          currentTop = -imgHeight;
-        } else if (currentTop < -imgHeight) {
+          currentTop = -vaixellHeight;
+        } else if (currentTop < -vaixellHeight) {
           currentTop = window.innerHeight;
         }
 
-        img.style.left = currentLeft + "px";
-        img.style.top = currentTop + "px";
+        vaixell.style.left = currentLeft + "px";
+        vaixell.style.top = currentTop + "px";
       });
+
+
 
       requestAnimationFrame(animate);
     }
@@ -66,8 +78,8 @@ window.onload = function () {
     window.addEventListener("keydown", function (event) {
       if (event.key >= "1" && event.key <= "5") {
         activeImg = parseInt(event.key) - 1;
-        if (activeImg >= imgs.length) {
-          activeImg = imgs.length - 1;
+        if (activeImg >= vaixells.length) {
+          activeImg = vaixells.length - 1;
         }
         webSocket.send(JSON.stringify({ key: "Number", value: event.key, activeImg: activeImg })); // Enviar el número presionado y activeImg al socket
       }
@@ -84,7 +96,7 @@ window.onload = function () {
          
       }
   
-      imgs[activeImg].style.transform = `rotate(${degrees[activeImg]}deg)`;
+      vaixells[activeImg].style.transform = `rotate(${degrees[activeImg]}deg)`;
     });
   
     webSocket.onmessage = function (event) {
@@ -104,6 +116,6 @@ window.onload = function () {
         
       }
   
-      imgs[activeImg].style.transform = `rotate(${degrees[activeImg]}deg)`;
+      vaixells[activeImg].style.transform = `rotate(${degrees[activeImg]}deg)`;
     };
   };
