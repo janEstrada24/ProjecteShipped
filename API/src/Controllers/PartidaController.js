9dc4c629ec627ca7b9d195e62e2973c2a030e47b
janEstrada24 <jestrada48@boscdelacoma.cat>
2024-03-13 20:58:33 +0100

    Directori "API/Database" afegit a .gitignore. Controladors "PartidaController.js" i "UsuariController.js" creats. Per últim, peticions "getUsuarisAsc" i "getUsuarisDesc" afegides a "UsuariController.js" i peticions "getPartidesActives" i "getPartidesFinalitzades" afegides a "PartidaController.js"

---
+++ API/src/Controllers/PartidaController.js
@@ -0,0 +1,26 @@
+const client = require("../Database/connection.js");
+
+const getPartidesActives = async (req, res) => {
+    const values = ['activa'];
+    const query = 'SELECT * FROM partides WHERE estat = $1';
+    
+    await client.query(query, values, (error, response) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json(response.rows);
+    });
+}
+
+
+const getPartidesFinalitzades = async (req, res) => {
+    const values = ['finalitzada'];
+    const query = 'SELECT * FROM partides WHERE estat = $1';
+    
+    await client.query(query, values, (error, response) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json(response.rows);
+    });
+}
---
+++ API/src/Controllers/UsuariController.js
@@ -0,0 +1,19 @@
+const client = require("../Database/connection.js");
+
+const getUsuarisAsc = async (req, res) => {
+    try {
+        const response = await client.query("SELECT * FROM usuaris ORDER BY Victories ASC, Empats ASC;");
+        res.status(200).json(response.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getUsuarisDesc = async (req, res) => {
+    try {
+        const response = await client.query("SELECT * FROM usuaris ORDER BY Victories DESC, Empats DESC;");
+        res.status(200).json(response.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
