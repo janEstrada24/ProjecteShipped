0ca9668a583c7ed63ecab0f2633dbfabbbf469e7
janEstrada24 <jestrada48@boscdelacoma.cat>
2024-03-15 11:26:04 +0100

    Problema per accedir a les variables d'entorn al projecte de l'API arreglat. S'ha aconseguit afegint el parámetre "--env-file=rutaFitxer.env" en el moment d'executar la comanda "start" (indicat al fitxer "package.json")

--- API/src/Database/connection.js
+++ API/src/Database/connection.js
@@ -1,19 +1,12 @@
 require("dotenv").config();
 const { Client } = require('pg');
 
-/*const client = new Client ({
+const client = new Client ({
     host: process.env.HOST_DATABASE,
     user: process.env.USER_DATABASE,
     port: process.env.PORT_DATABASE,
     password: process.env.PASSWORD_DATABASE,
     database: process.env.DATABASE
-});*/
-const client = new Client ({
-    host: "localhost",
-    user: "admin",
-    port: "5432",
-    password: "8*U9l9",
-    database: "admin"
 });
 
 module.exports = client;
